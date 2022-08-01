314823ce278c758abd83f2befd180a5e1505b58d
bendanykykyk <446679223@qq.com>
2022-08-01 11:21:37 +0800

    ✨ feat(项目中封装axios,以及页面中简单使用):

--- src/pages/discover/recommend/index.js
+++ src/pages/discover/recommend/index.js
@@ -1,6 +1,13 @@
-import React, {memo} from "react";
+import React, {memo, useEffect} from "react";
+
+import {getTopBanner} from "@/services/recommend";
 
 const YKRecommend = memo(() => {
+  useEffect(() => {
+    getTopBanner().then((res) => {
+      console.log(res);
+    });
+  });
   return <div>YKRecommend</div>;
 });
 
